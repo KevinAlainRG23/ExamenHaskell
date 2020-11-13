bdef877c31270ff224522aa3356ae3b79b4b4e26
KevinAlainRG23 <71893537+KevinAlainRG23@users.noreply.github.com>
2020-11-13 10:59:26 -0600

    Create Examen.hs

---
+++ Examen.hs
@@ -0,0 +1,68 @@
+main = do
+        funcionDia
+
+
+funcionDia = do
+            putStrLn("INSERTE EL DIA") 
+            b <- getLine  
+            let bInt= read b::Int
+            if (bInt>=1 || bInt<=31)
+                then do
+                putStrLn("naciste el dia: "++show bInt) 
+            else if bInt>=1 || bInt<=30
+                then 
+                putStrLn("naciste el dia: "++show bInt) 
+            else
+                putStrLn("salir")
+            
+            funcionMes
+
+
+
+
+
+funcionMes = do
+            putStrLn("INSERTE EL MES DE NACIMIENTO") 
+            a <- getLine  
+            let aInt= read a::Int
+            if (aInt==1)
+                then do
+                putStrLn("naciste en ENERO, tu mes tiene 31 dia")     
+            else if aInt==2
+                then 
+                putStrLn("naciste en FEBRERO, tu mes tiene 30 dias")    
+            else if aInt==3
+                then 
+                putStrLn("naciste en MARZO, tu mes tiene 31 dia")  
+            else if aInt==4
+                then 
+                putStrLn("naciste en ABRIL,  tu mes tiene 30 dias")  
+            else if aInt==5
+                then 
+                putStrLn("naciste en MAYO, tu mes tiene 31 dia") 
+            else if aInt==6
+                then 
+                putStrLn("naciste en JUNIO,  tu mes tiene 30 dias") 
+            else if aInt==7
+                then 
+                putStrLn("naciste en JULIO, tu mes tiene 31 dia") 
+            else if aInt==8
+                then 
+                putStrLn("naciste en AGOSTO,  tu mes tiene 31 dias") 
+            else if aInt==9
+                then 
+                putStrLn("naciste en SEPTIEMBRE,  tu mes tiene 30 dias") 
+            else if aInt==10
+                then 
+                putStrLn("naciste en OCTUBRE, tu mes tiene 31 dia") 
+            else if aInt==11
+                then 
+                putStrLn("naciste en NOVIEMBRE,  tu mes tiene 30 dias") 
+            else if aInt==12
+                then 
+                putStrLn("naciste en DICIEMBRE, tu mes tiene 31 dia") 
+            else if aInt==13 || aInt>13
+                then 
+                putStrLn("Estes mes no existe") 
+            else
+                putStrLn("salir") 
